f97bbab556e61c414591fa6c497dbb38e00ee445
toto <toto6321@qq.com>
2020-06-10 17:53:14 -0400

    feat: add sections os and software

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,27 +1,49 @@
 <template>
-    <v-container class="d-flex flex-column skill">
-        <h2>Skill</h2>
-        <div class="pa-2 skill-table">
-            <h3># Programming</h3>
+    <div class="skill d-flex flex-column mt-2">
+        <h2># Skill</h2>
+        <div class="skill-wrapper wrapper">
+            <h3>## Programming</h3>
             <div class="d-flex flex-column programming-wrapper">
-                <div v-for="(category, i) in data.programming" :key="i">
-                    <div class="d-flex programming-item">
+                <div v-for="(category, i) in data.programming" :key="i"
+                     class=""
+                >
+                    <div class="d-flex category-item">
                         <div class="d-flex align-center skill-left">
                             {{category[0].category}}
                         </div>
-                        <div class="flex-row skill-right">
-                            <v-container v-for="(lang, i) in category" :key="i"
-                                         class="d-flex px-2">
+                        <div class="flex-row programming-item-right">
+                            <div v-for="(lang, i) in category" :key="i"
+                                 class="d-flex programming-item">
                                 <label class="slider-label">{{lang.language}}</label>
                                 <input type="range" :value="lang.proficiency" max="10"
                                        class="slider"/>
-                            </v-container>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </v-container>
+        <div class="os-wrapper wrapper">
+            <h3>## OS</h3>
+            <div>
+                <ul class="ml-2">
+                    <li v-for="(e, i) in data.os" :key="i">
+                        <p>{{stringify(e)}}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="software-wrapper wrapper">
+            <h3>## Software</h3>
+            <div>
+                <ul class="ml-2">
+                    <li v-for="(e, i) in data.software" :key="i">
+                        <p>{{stringify(e)}}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -57,46 +79,82 @@
                                         "proficiency": 7
                                     }
                                 ]
+                            ],
+                        "os": [
+                            [
+                                "Ubuntu",
+                                "Kali"
+                            ],
+                            [
+                                "ROS"
+                            ],
+                            [
+                                "Windows 10",
+                                "Windows 7"
+                            ],
+                            [
+                                "Mac OS"
                             ]
+                        ],
                     }
                 }
             }
+        },
+        data: function () {
+            return {}
+        },
+        methods: {
+            stringify: (item) => Array.isArray(item) ? item.join(', ') : item.toString()
         }
 
     }
 </script>
 
-<style scoped>
-    .skill {
-        min-width: 290px;
-        max-width: 360px;
-    }
+<style scoped lang="sass">
+    *
+        line-height: 1
 
-    .programming-wrapper {
-        line-height: 1;
-    }
 
-    .programming-item {
-        max-width: 100%
-    }
+        .skill
+            padding-left: 8px
+            padding-right: 8px
 
-    .skill-left {
-        flex: 0 0 30%;
-        max-width: 30%;
-    }
 
-    .skill-right {
-        flex: 0 0 70%;
-        max-width: 70%;
-    }
+        .wrapper
+            padding-left: 8px
 
-    .slider-label {
-        min-width: 6rem;
-        max-width: 50%;
-    }
+            .programming-wrapper
 
-    .slider {
-        max-width: 60%;
-        color: blue;
-    }
+                .programming-item
+                    margin: 4px 0
+
+                    .programming-item-left
+                        flex: 0 0 30%
+                        max-width: 30%
+
+
+                    .programming-item-right
+                        flex: 0 0 60%
+                        max-width: 70%
+
+                        .slider-label
+                            max-width: 50%
+
+
+                        .slider
+                            max-width: 50%
+                            color: blue
+
+        h2, h3
+            margin: 8px 0
+
+
+        li
+            margin-bottom: 4px
+
+
+        p
+            margin-bottom: 4px
+
+
 </style>
