1bbb8fdadf64d6a733ad298f8a71a3d0f0f600dd
toto <toto6321@qq.com>
2020-06-11 00:22:42 -0400

    refactor: use progress-linear instead of slider

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,22 +1,21 @@
 <template>
     <div class="skill d-flex flex-column mt-2">
         <h2># Skill</h2>
-        <div class="skill-wrapper wrapper">
+        <div class="programming-wrapper wrapper">
             <h3>## Programming</h3>
             <div class="d-flex flex-column programming-wrapper">
                 <div v-for="(category, i) in data.programming" :key="i"
-                     class=""
+                     class="programming-item"
                 >
                     <div class="d-flex category-item">
-                        <div class="d-flex align-center skill-left">
+                        <div class="programming-item-left d-flex align-center">
                             {{category[0].category}}
                         </div>
                         <div class="flex-row programming-item-right">
                             <div v-for="(lang, i) in category" :key="i"
                                  class="d-flex programming-item">
-                                <label class="slider-label">{{lang.language}}</label>
-                                <input type="range" :value="lang.proficiency" max="10"
-                                       class="slider"/>
+                                <label class="progress-label">{{lang.language}}</label>
+                                <v-progress-linear :value="lang.proficiency" class="progress"/>
                             </div>
                         </div>
                     </div>
@@ -59,24 +58,24 @@
                             [
                                 [
                                     {
-                                        "language": "Java",
-                                        "icon": "mdi-java",
-                                        "category": "backend",
-                                        "proficiency": 6
-                                    }
-                                ],
-                                [
-                                    {
                                         "language": "Vue.js",
                                         "icon": "mdi-vue",
                                         "category": "frontend",
-                                        "proficiency": 8
+                                        "proficiency": 80
                                     },
                                     {
                                         "language": "React.js",
                                         "icon": "mdi-react",
                                         "category": "frontend",
-                                        "proficiency": 7
+                                        "proficiency": 70
+                                    }
+                                ],
+                                [
+                                    {
+                                        "language": "Java",
+                                        "icon": "mdi-java",
+                                        "category": "backend",
+                                        "proficiency": 50
                                     }
                                 ]
                             ],
@@ -123,27 +122,31 @@
         .wrapper
             padding-left: 8px
 
-            .programming-wrapper
 
-                .programming-item
-                    margin: 4px 0
+        .programming-wrapper
 
-                    .programming-item-left
-                        flex: 0 0 30%
-                        max-width: 30%
+            .programming-item
+                margin: 4px 0
+
+                .programming-item-left
+                    flex: 0 0 25%
+                    max-width: 33%
 
 
-                    .programming-item-right
+                .programming-item-right
+                    flex: 0 0 75%
+                    max-width: 75%
+
+                    .progress-label
+                        flex: 0 0 40%
+                        max-width: 50%
+
+
+                    .progress
                         flex: 0 0 60%
-                        max-width: 70%
+                        min-width: 50%
+                        color: blue
 
-                        .slider-label
-                            max-width: 50%
-
-
-                        .slider
-                            max-width: 50%
-                            color: blue
 
         h2, h3
             margin: 8px 0
