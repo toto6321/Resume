3b877ca35d6cfe8303cb1a0e950ed5d3f8047615
toto <toto6321@qq.com>
2020-06-11 00:46:16 -0400

    feat: implement component Introduction.vue

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        Introduction
-    </div>
+    <v-card>
+        <v-card-title>{{career}}</v-card-title>
+        <v-card-text>
+            <ul>
+                <li v-for="(e, i) in data.about" :key="i">
+                    <p> {{e}}</p>
+                </li>
+            </ul>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
     export default {
-        name: "Introduction"
+        name: "Introduction",
+        props: ['data'],
+        computed: {
+            career: function () {
+                const prefix = 'CAREER FOCUS'
+                return (prefix + ': ' + this.data.career).toUpperCase()
+            }
+        }
     }
 </script>
 
