c6e346002dd7d5eb5a3a7dfcf0683d32f957c364
toto <toto6321@qq.com>
2020-06-17 18:24:13 -0400

    feat: add data template

---
+++ src/data.template.js
@@ -0,0 +1,280 @@
+module.exports = {
+    "profile": {
+        "name":
+            "Apple Bee",
+        "items": [
+            {
+                "name": "degree",
+                "icon": "mdi-certificate",
+                "value": "Master of Science in CS"
+            },
+            {
+                "name": "language",
+                "icon": "mdi-translate",
+                "value": ["English"]
+            },
+            {
+                "name": "phone",
+                "icon": "mdi-phone",
+                "value": "+1 123 456 7890"
+            },
+            {
+                "name": "address",
+                "icon": "mdi-home",
+                "value": "AAA, BBB, US"
+            },
+            {
+                "name": "email",
+                "icon": "mdi-email-outline",
+                "value": "applebee@gmail.com"
+            },
+            {
+                "name": "github",
+                "icon": "mdi-github-circle",
+                "value": "https://gihub.com/applebee"
+            },
+            {
+                "name": "linkedin",
+                "icon": "mdi-linkedin",
+                "value": "https://www.linkedin.com/"
+            },
+            {
+                "name": "link",
+                "icon": "mdi-link",
+                "value": "https://wix.com"
+            }
+        ]
+    },
+    "skill": {
+        "programming": [
+            [
+                {
+                    "language": "Vue.js",
+                    "icon": "mdi-language-vue",
+                    "category": "frontend",
+                    "proficiency": 80
+                },
+                {
+                    "language": "React.js",
+                    "icon": "mdi-language-react",
+                    "category": "frontend",
+                    "proficiency": 70
+                }
+            ],
+            [
+                {
+                    "language": "Nodejs",
+                    "icon": "mdi-language-nodejs",
+                    "category": "backend",
+                    "proficiency": 75
+                },
+                {
+                    "language": "Java",
+                    "icon": "mdi-language-java",
+                    "category": "backend",
+                    "proficiency": 50
+                }
+            ],
+        ],
+        "os": [
+            [
+                "Ubuntu",
+                "Kali"
+            ],
+            [
+                "ROS"
+            ],
+            [
+                "Windows 10",
+                "Windows 7"
+            ],
+            [
+                "Mac OS"
+            ]
+        ],
+        "software": [
+            [
+                "VSCode",
+                "Sublime",
+                "Notepad++",
+                "Atom"
+            ],
+            [
+                "IDEA",
+                "Eclipse",
+                "NetBeans"
+            ],
+            [
+                "PyCharm",
+                "Jupyter Notebook",
+                "Spyder"
+            ],
+            [
+                "WebStorm",
+                "Clion"
+            ]
+        ]
+    },
+    "introduction": {
+        "career": "Web developer",
+        "about": [
+            "A passionate, fast, and efficient learner in new techniques",
+            "An enthusiastic and patient team player to collaborate with and make friends"
+        ]
+    },
+    "knowledge": [
+        [
+            "Linux",
+            "Bash",
+            "Container",
+            "Docker",
+            "Kubernetes",
+            "Jenkins",
+            "Gitlab",
+            "Travis"
+        ],
+        [
+            "Git",
+            "Git Flow",
+            "Github Flow",
+            "Markdown"
+        ]
+    ],
+    "course": [
+        {
+            "name": "Operating System",
+            "grade": null,
+            "project": {
+                "name": "Term project",
+                "link": "https://google.com"
+            }
+        },
+        {
+            "name": "Operating System",
+            "grade": null,
+            "project": {
+                "name": "Term project",
+                "link": "https://google.com"
+            }
+        },
+        {
+            "name": "Operating System",
+            "grade": null,
+            "project": {
+                "name": "Term project",
+                "link": "https://google.com"
+            }
+        },
+        {
+            "name": "Operating System",
+            "grade": null,
+            "project": {
+                "name": "Term project",
+                "link": "https://google.com"
+            }
+        }
+    ],
+    "work": [
+        {
+            "employer": {
+                "name": "Oakland University",
+                "city": "Rochester, Mi, US",
+                "link": "https://www.oakland.edu"
+            },
+            "start_time": "2030-09",
+            "end_time": "2032-04",
+            "position": "Teaching Assistant",
+            "achievement": [
+                "Assisted lectures, helped design lab activities, supervised technical labs, and served as a mentor in courses: Object-Oriented Programming"
+            ]
+        },
+        {
+            "employer": {
+                "name": "Amazon",
+                "city": "Detroit, Michigan, US",
+                "link": "https://amazon.com"
+            },
+            "start_time": "2020-01",
+            "end_time": "2029-12",
+            "position": "Web developer",
+            "achievement": [
+                "Fixed bugs and developed new feature"
+            ]
+        }
+    ],
+    "education": [
+        {
+            "school": {
+                "name": "Oakland University",
+                "link": "https://www.oakland.edu",
+                "city": "Rochester, Mi, US",
+                "zip_code": "48309"
+            },
+            "major": "Computer Science",
+            "degree": "Master",
+            "status": "completed",
+            "period": {
+                "start_time": "2018-09",
+                "end_time": "2020-04"
+            }
+        },
+        {
+            "school": {
+                "name": "Oakland University",
+                "link": "https://www.oakland.edu",
+                "city": "Rochester, Mi, US",
+                "zip_code": "48309"
+            },
+            "major": "Computer Science",
+            "degree": "Bachelor",
+            "status": "completed",
+            "period": {
+                "start_time": "2016-09",
+                "end_time": "2020-04"
+            }
+        }
+    ],
+    "repository":
+        [
+            {
+                "name": "Frontend for https://feiyan.help",
+                "technical_stack": [
+                    {
+                        "name": "vue.js",
+                        "logo": "https://vuejs.org/images/logo.png?_sw-precache=cf23526f451784ff137f161b8fe18d5a"
+                    },
+                    {
+                        "name": "vuetify",
+                        "logo": "https://raw.githubusercontent.com/vuetifyjs/awesome-vuetify/master/logo.png"
+                    },
+                    {
+                        "name": "docker",
+                        "logo": "https://www.docker.com/sites/default/files/d8/styles/role_icon/public/2019-07/vertical-logo-monochromatic.png?itok=erja9lKc"
+                    }
+                ],
+                "description": "https://feiyan.help is open-source and maintained by non-profit organization wuhan-support, aiming at helping provide instant information to residents, volunteers, doctors and hospitals in Wuhan, China during the COVID-2019 epidemic"
+            },
+            {
+                "name": "Resume",
+                "technical_stack": [
+                    {
+                        "name": "vue.js",
+                        "logo": "https://vuejs.org/images/logo.png?_sw-precache=cf23526f451784ff137f161b8fe18d5a"
+                    },
+                    {
+                        "name": "vuetify",
+                        "logo": "https://raw.githubusercontent.com/vuetifyjs/awesome-vuetify/master/logo.png"
+                    },
+                    {
+                        "name": "React.js",
+                        "logo": "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+                    },
+                    {
+                        "name": "Element-UI",
+                        "logo": "https://element.eleme.cn/favicon.ico"
+                    }
+                ],
+                "description": "This resume actually is a web page, intended to be compact for print. You can also visit https://secs.oakland.edu/~applebee to see more details."
+            },
+        ]
+}
