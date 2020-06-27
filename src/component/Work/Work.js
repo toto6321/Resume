88e7da766c1db556ab2d78490b63e6fa20bd2aaf
toto <toto6321@qq.com>
2020-06-27 16:37:11 -0400

    style: work style

--- src/component/Work/Work.js
+++ src/component/Work/Work.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Work.sass'
 
 function Work(props) {
 
@@ -14,33 +15,37 @@ function Work(props) {
     return (
         <div className="v-card">
             <div className="v-card__title"><h2>{props.title}</h2></div>
-            <div className="v-card__text">
+            <div className="v-card__text flex-column">
                 {
                     props.data.map((e, i) => (
-                        <div key={i}>
-                            <div>
-                                <div className="year">{parse_date_string(e.end_time).year}</div>
-                                <div className="month">{parse_date_string(e.end_time).month}</div>
-                                <div>
+                        <div key={i} className="flex-column work">
+                            <div className="flex">
+                                <div className="year text-center">{parse_date_string(e.end_time).year}</div>
+                                <div className="month text-center">{parse_date_string(e.end_time).month}</div>
+                                <div className="detail flex">
                                     <div><h3>{e.position}</h3></div>
                                     <div>{e.employer.city}</div>
                                 </div>
+                            </div>
+                            <div className="flex">
+                                <div className="year text-center">{parse_date_string(e.start_time).year}</div>
+                                <div className="month text-center">{parse_date_string(e.start_time).month}</div>
+                                <div className="detail">
+                                    <a href={e.employer.link}>{e.employer.name}</a>
                                 </div>
-                                <div>
-                                    <div className="year">{parse_date_string(e.start_time).year}</div>
-                                    <div className="month">{parse_date_string(e.start_time).month}</div>
-                                    <div><a href={e.employer.link}>{e.employer.name}</a></div>
-                                </div>
-                                <div>
-                                    <div className="year dash"/>
-                                    <div className="month"/>
-                                    <div className="detail">
+                            </div>
+                            <div className="flex">
+                                <div className="year dash"/>
+                                <div className="month"/>
+                                <div className="detail">
+                                    <ul>
                                         {
                                             e.achievement.map((e2, i2) => {
                                                 return <li key={i2}>{e2}</li>
                                             })
                                         }
-                                    </div>
+                                    </ul>
+                                </div>
                                 </div>
                             </div>
                         )
