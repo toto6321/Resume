24873ea786be6fea90004076e4bda97d07b00f37
toto <toto6321@qq.com>
2020-06-27 16:50:58 -0400

    style: education style

--- src/component/Education/Education.js
+++ src/component/Education/Education.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Education.sass'
 
 function Education(props) {
 
@@ -6,21 +7,23 @@ function Education(props) {
         <div className="v-card">
             <div className="v-card__title"><h2>{props.title}</h2></div>
             <div className="v-card__text">
-                {
-                    props.data.map((e, i) => (
-                        <div key={i}>
-                            <div>
-                                <a href={e.school.link}>
-                                    <img alt={e.school.name} src={e.school.icon}/>
-                                </a>
+                <div className="flex">
+                    {
+                        props.data.map((e, i) => (
+                            <div key={i} className="education">
+                                <div className="school-logo">
+                                    <a href={e.school.link}>
+                                        <img alt={e.school.name} src={e.school.icon}/>
+                                    </a>
+                                </div>
+                                <div className="description">
+                                    <div><h3>{e.school.name}</h3></div>
+                                    <div><h4>{e.degree}</h4></div>
+                                </div>
                             </div>
-                            <div className="description">
-                                <div><h3>{e.school.name}</h3></div>
-                                <div><h4>{e.degree}</h4></div>
-                            </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
