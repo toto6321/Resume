c4ab37b4cbb95d5626c35dab44392ef8b7d02399
toto <toto6321@qq.com>
2020-06-27 17:01:44 -0400

    style: repository style

--- src/component/Repository/Repository.js
+++ src/component/Repository/Repository.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Repository.sass'
 
 function Repository(props) {
 
@@ -6,28 +7,30 @@ function Repository(props) {
         <div className="v-card">
             <div className="v-card__title"><h2>{props.title}</h2></div>
             <div className="v-card__text">
-                {
-                    props.data.map((e, i) => (
-                        <div key={i}>
-                            <div>
-                                <h3>{e.name}</h3>
-                            </div>
-                            <div>
-                                {
-                                    e.technical_stack.map((e2, i2) => (
-                                        <a key={i2} href={e2.link}>
-                                            <img alt={e2.name} src={e2.logo}/>
-                                        </a>
-                                    ))
-                                }
-                            </div>
-                            <div>
-                                {e.description}
-                            </div>
-                        </div>
+                <div className="repository_wrapper flex">
+                    {
+                        props.data.map((e, i) => (
+                                <div key={i} className="repository">
+                                    <div className="mb-1">
+                                        <h3>{e.name}</h3>
+                                    </div>
+                                    <div className="mb-1">
+                                        {
+                                            e.technical_stack.map((e2, i2) => (
+                                                <a key={i2} href={e2.link}>
+                                                    <img alt={e2.name} src={e2.logo}/>
+                                                </a>
+                                            ))
+                                        }
+                                    </div>
+                                    <div>
+                                        {e.description}
+                                    </div>
+                                </div>
+                            )
                         )
-                    )
-                }
+                    }
+                </div>
             </div>
         </div>
     )
